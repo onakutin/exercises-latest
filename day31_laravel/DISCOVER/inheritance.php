207ad997d8b7e449c0012b87dc24051189ed7758
Ondrej.Kutina <onakutin@protonmail.com>
2023-10-18 21:52:46 +0200

    forgotten save in inheritance

--- day31_laravel/DISCOVER/inheritance.php
+++ day31_laravel/DISCOVER/inheritance.php
@@ -1,6 +1,6 @@
 <?php
 
-class Match
+class MatchClass
 {
     public $begins_at = null;
     public $score = null;
@@ -9,7 +9,7 @@ class Match
     public function __construct($time)
     {
         $this->begins_at = $time;
-    } 
+    }
 
     public function getEstimatedEnd($begins_at)
     {
@@ -17,5 +17,25 @@ class Match
         $ends_at_timestamp = $begins_at_timestamp + (60 * $this->length);
         $ends_at_string = date('Y-m-d H:i:s', $ends_at_timestamp);
         return $ends_at_string;
+    }
+}
+
+class FootballMatch extends MatchClass
+{
+    public $nr_of_players = 22;
+    public $length = 90;
+    public $halves = ['0:0', '0:0'];
+    public $nr_offsides = '0';
+}
+
+class IcehockeyMatch extends MatchClass
+{
+    public $nr_of_players = 12;
+    public $length = 60;
+    public $thirds = ['0:0', '0:0', '0:0'];
+    public function getThirdScore($third)
+    {
+        $thirdScore = $this->thirds[$third];
+        return ($thirdScore);
     }
 }
